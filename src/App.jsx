e31773f32e96cedefff463508a3d38006304ac93
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-08-04 20:08:18 +0800

    refactor: handle edit by creating stand alone state

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,36 @@ export default function App() {
   const [taskName, setTaskName] = useState("")
   const [taskList, setTaskList] = useState([])
   const [alert, setAlert] = useState("idle")
+  const taskRef = useRef(null)
 
-  const inputRef = useRef(null)
+  const [editingId, setEditingId] = useState(null)
+
+  // function handleEdit(index){
+  //   const selectedTask = taskList[index]
+  //   setAdd(prev=> ({...prev, taskPrompt: true, isEdit: true}))
+  //   setTaskName(selectedTask.text)
+  //   setEditingId(selectedTask.id)
+  //   return setAlert("edit")
+  // }
+
+  useEffect(()=>{
+    if (add.taskPrompt || add.isEdit){
+      console.log("useEffect")
+      taskRef.current.focus()
+    }
+  }, [add.taskPrompt, add.isEdit])
 
   function handleDone(id){
     setTaskList(prev=> prev.map(item=> item.id === id ? ({...item, isDone: !item.isDone}) : item))
     console.log(taskList)
   };
-
-  useEffect(()=> {
-    if (add.taskPrompt){
-      inputRef.current.focus();
-    }
-    },[add.taskPrompt])
-
   function handleChange(e){
     return setTaskName(e.target.value)
   }
   function addBtn(){
-    console.log(inputRef.current, "true")
     setAlert("idle");
     setTaskName("");
-    return setAdd(()=> ({taskPrompt: true, isEdit: false}))
+    return setAdd((prev)=> ({...prev, taskPrompt: true, isEdit: false}))
   }
   function handleDelete(id){
     setAlert("idle");
@@ -40,11 +48,11 @@ export default function App() {
     setTaskName("")
   }
   function handleEdit(index){
-    const selectedTask = taskList[index].text
+    const selectedTask = taskList[index];
     setAdd(prev=> ({...prev, taskPrompt: true, isEdit: true}))
-    setTaskName(selectedTask)
-    console.log(add, selectedTask)
-    return setAlert("edit")
+    setTaskName(selectedTask.text)
+    setEditingId(selectedTask.id);
+    console.log("handle edit", selectedTask)
   }
   function handleCancel(){
     setAdd(prev=> ({...prev, taskPrompt: false, isEdit: false}));
@@ -71,7 +79,7 @@ export default function App() {
     }
     console.log("movedown")
   }
-  function handleSubmit(e, index){
+  function handleSubmit(e){
     e.preventDefault();
     const tTask = taskName.trim();
     const isDuplicate = taskList.some(item=> item.text === tTask.toLowerCase())
@@ -82,9 +90,9 @@ export default function App() {
       return setAlert("duplicate");
     }
     if (add.isEdit === true){
-      setTaskList(prev=> prev.map(item=> item.id !== index ? {...item, text: tTask} : item));
-      setAdd(()=> ({taskPrompt: false, isEdit: false}))
-      return setTaskName("") 
+      setTaskName("");
+      setAdd(()=> ({taskPrompt: false, isEdit: false}));
+      return setTaskList(prev=> prev.map(item=> item.id === editingId ? {...item, text: tTask} : item))
     }
     const newTask = {id: crypto.randomUUID(), text: tTask, isDone: false};
     setAlert("idle")
@@ -92,6 +100,28 @@ export default function App() {
     setTaskName("")
     setAdd((prev)=> ({...prev, taskPrompt: false}))
   }
+
+// App.jsx
+
+  // function handleSubmit(e){
+  //   e.preventDefault();
+  //   const tTask = taskName.trim();
+  //   const isDuplicate = taskList.some(item=> item.text === tTask.toLowerCase())
+  //   if (tTask === "") return setAlert("blank")
+  //   if (isDuplicate) return setAlert("duplicate");
+
+  //   if (add.isEdit === true){
+  //     setTaskList(prev=> prev.map(item=> item.id === editingId ? {...item, text: tTask} : item));
+  //     setAdd(()=> ({taskPrompt: false, isEdit: false}))
+  //     setEditingId(null)
+  //     return setTaskName("")
+  //   }
+  //   const newTask = {id: crypto.randomUUID(), text: tTask, isDone: false};
+  //   setAlert("idle")
+  //   setTaskList((prev)=>[...prev, newTask])
+  //   setTaskName("")
+  //   setAdd((prev)=> ({...prev, taskPrompt: false}))
+  // }
 return (
   <section className="flex flex-col px-5 bg-slate-900 text-white overflow-hidden">
     <section className="flex items-center justify-center">
@@ -100,11 +130,11 @@ return (
     </section>
     <div>
       <section className="flex">
-        <section className="py-5 px-3">
+        <section className="py-5 px-3 ">
           <TaskList alert={alert} taskList={taskList} handleMoveUp={handleMoveUp} handleMoveDown={handleMoveDown} handleEdit={handleEdit} handleDelete={handleDelete} handleDone={handleDone} />
         </section>
         <section className="py-5 px-3">
-          {add.taskPrompt && <AddTaskComponent inputRef={inputRef} taskName={taskName} handleChange={handleChange} handleSubmit={handleSubmit} handleCancel={handleCancel} alert={alert} />}
+          {add.taskPrompt && <AddTaskComponent taskRef={taskRef} taskName={taskName} handleChange={handleChange} handleSubmit={handleSubmit} handleCancel={handleCancel} alert={alert} />}
           <InProg className="pb-10" />
         </section>
         <section className="py-5 px-3">
--- src/components/TaskComponent/AddTaskComponent.jsx
+++ src/components/TaskComponent/AddTaskComponent.jsx
@@ -1,12 +1,12 @@
 import TaskBtn from "./AddTaskBtn.jsx";
 import Alert from './Alert.jsx'
 // section for registering new task
-export default function AddTaskComponent({taskName, inputRef, handleChange,handleCancel, handleSubmit, alert}) {
+export default function AddTaskComponent({taskRef, taskName, handleChange,handleCancel, handleSubmit, alert}) {
   return (
     <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center h-screen z-50 bg-gray-500">
         <Alert alert={alert} />
         <h1 className="p-5 text-black">Welcome, pls input your task below!</h1>
-        <input ref={inputRef} type="text"  className="border p-5 rounded-3xl text-black" placeholder="Enter your task here..." name="taskName" value={taskName} onChange={(e)=> handleChange(e)} />
+        <input ref={taskRef} type="text" className="border p-5 rounded-3xl text-black" placeholder="Enter your task here..." name="taskName" value={taskName} onChange={(e)=> handleChange(e)} />
         <TaskBtn handleCancel={handleCancel} handleSubmit={handleSubmit} />
     </form>
   )
