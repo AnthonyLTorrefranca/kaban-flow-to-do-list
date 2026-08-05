4a7eefd347ef726dc9984063645b1b3af3bb61c1
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-08-05 20:21:44 +0800

    refactor: transfer to Done.jsx the done tasks

--- src/components/TaskComponent/Alert.jsx
+++ src/components/TaskComponent/Alert.jsx
@@ -3,7 +3,7 @@ export default function Alert({alert}) {
     <section>
       {alert === "idle" && <p className="text-2xl">Task List</p>}
       {alert === "blank" && <p className="text-2xl text-red-500">Task cannot be blank!</p>}
-      {alert === "top" && <p className="text-2xl text-red-500">All the way down!</p>}
+      {alert === "top" && <p className="text-2xl text-red-500">All the way up!</p>}
       {alert === "down" && <p className="text-2xl text-red-500">All the way down!</p>}
       {alert === "duplicate" && <p className="text-2xl text-red-500">Duplicate Task!</p>}
       {alert === "edit" && <p className="text-2xl">Edit task mode.</p>}
